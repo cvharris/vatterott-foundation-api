f5d123dd15101a059f5e2dd405571ef4e4118333
harrisch <charlie.harris@wwt.com>
2016-12-27 14:52:53 -0600

    Adds route for listing current applications

--- app/applications/applicationController.js
+++ app/applications/applicationController.js
@@ -1,6 +1,10 @@
 "use strict";
 
 const co = require('co')
+const fs = require('fs')
+const path = require('path')
+const root = path.dirname(require.main.filename)
+const _ = require('lodash')
 
 module.exports = function grantControllerFactory(Application, log) {
 
@@ -12,11 +16,56 @@ module.exports = function grantControllerFactory(Application, log) {
   }
 
   function* list(request, reply) {
-    let queryParams = {}
+    // let queryParams = {}
+    //
+    // const applications = yield Application.find()
+    //
+    // return reply(applications)
 
-    const applications = yield Application.find()
+    const fileDir = `${root}/grantApplications`
+    log.info('browsing:', {
+      fileDir: fileDir
+    })
 
-    return reply(applications)
+    fs.readdir(fileDir, (err, files) => {
+      if (err) {
+        throw err
+      }
+
+      let data = []
+
+      files.forEach((file) => {
+        try {
+          log.info('processing file', {
+            file: file
+          })
+
+          let isDirectory = fs.statSync(path.join(fileDir, file)).isDirectory()
+          if (isDirectory) {
+            data.push({
+              name: file,
+              isDirectory: true,
+              path: path.join(fileDir, file)
+            })
+          } else {
+            let ext = path.extname(file)
+
+            data.push({
+              name: file,
+              ext: ext,
+              isDirectory: false,
+              path: path.join(fileDir, file)
+            })
+          }
+        } catch(e) {
+          log.error(e)
+        }
+      })
+
+      data = _.sortBy(data, f => f.name)
+
+      reply(data)
+    })
 	}
 
   function* findByName(request, reply) {
